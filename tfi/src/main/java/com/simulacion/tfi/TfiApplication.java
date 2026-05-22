70b0a3e3faa90087a0aa946f1d50795acf9b5aeb
nicolas cabaleiro <nicocabaleiro2002@gmail.com>
2026-05-21 21:49:08 -0300

    first commit

---
+++ tfi/src/main/java/com/simulacion/tfi/TfiApplication.java
@@ -0,0 +1,13 @@
+package com.simulacion.tfi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfiApplication.class, args);
+	}
+
+}
